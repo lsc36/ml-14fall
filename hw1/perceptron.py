747f283bd31d25a1246c21b21ea8de38dd9faa86
Shao-Chuan Lee <lsc36x@gmail.com>
2014-10-01 17:01:55 +0800

    implement hw1.15

---
+++ hw1/perceptron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+def sgn(x):
+    if x > 0: return 1.0
+    return -1.0
+
+
+def dot(x, y):
+    return sum([a * b for (a, b) in zip(x, y)])
+
+
+def train(data):
+    dimensions = len(data[0]) - 1
+    vec = [0.0] * dimensions
+    updates = 0
+    while True:
+        mistake = False
+        for entry in data:
+            if sgn(dot(vec, entry[:dimensions])) != entry[dimensions]:
+                mistake = True
+                vec = [w + entry[dimensions] * x for (w, x) in zip(vec, entry[:dimensions])]
+                break
+        if not mistake: break
+        updates = updates + 1
+    return (vec, updates)
+
+
+def main():
+    f = open('data/hw1_15_train.dat')
+    data = [[float(x) for x in ['1.0'] + line.split()] for line in f.readlines()]
+    f.close()
+
+    print("w = %s\nupdates = %d" % train(data))
+
+
+if __name__ == "__main__":
+    main()
