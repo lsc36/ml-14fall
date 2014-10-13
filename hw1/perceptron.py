9d4d689dbd99c356c8119c66cc2a864366117dc4
Shao-Chuan Lee <lsc36x@gmail.com>
2014-10-14 00:59:22 +0800

    use argv to choose problem num

--- hw1/perceptron.py
+++ hw1/perceptron.py
@@ -25,12 +25,22 @@ def train(data):
     return (vec, updates)
 
 
-def main():
+def prob15():
     f = open('data/hw1_15_train.dat')
     data = [[float(x) for x in ['1.0'] + line.split()] for line in f.readlines()]
     f.close()
-
     print("w = %s\nupdates = %d" % train(data))
+
+
+def main():
+    fmap = {
+        '15': prob15,
+    }
+    import sys
+    try:
+        fmap[sys.argv[1]]()
+    except:
+        print("Usage: %s <prob_num>" % sys.argv[0])
 
 
 if __name__ == "__main__":
