29a4ede82bdbea59dea8c38f662144927f7046db
Shao-Chuan Lee <lsc36x@gmail.com>
2014-10-29 10:16:32 +0800

    prob 19, 20

--- hw2/stump.py
+++ hw2/stump.py
@@ -21,13 +21,13 @@ def stump(dataset):
     err_min = len(dataset)
     list_st = []
     for s in [1, -1]:
-        for t in range(21):
+        for t in range(len(dataset) + 1):
             err = 0
             for i in range(t):
                 if dataset[i][1] != -s: err = err + 1
-            for i in range(t, 20):
+            for i in range(t, len(dataset)):
                 if dataset[i][1] != s: err = err + 1
-            if t == 0: theta = -1.0
+            if t == 0: theta = dataset[0][0] - 0.000001
             else: theta = dataset[t - 1][0]
             if err_min > err:
                 err_min = err
@@ -55,10 +55,37 @@ def prob1718():
             for (err, (s, t)) in res_list]) / 5000))
 
 
+def read_data(filename):
+    f = open(filename)
+    data = [[float(x) for x in line.split()] for line in f.readlines()]
+    f.close()
+    return data
+
+
+def prob1920():
+    d_train = read_data("data/hw2_train.dat")
+    dim = len(d_train[0]) - 1
+    list_est = []
+    for i in range(dim):
+        list_est.append((stump([(d[i], d[dim]) for d in d_train]), i))
+    list_est.sort()
+    list_est_min = [est for est in list_est if est[0] == list_est[0][0]]
+    (err, (s, t)), i = random.choice(list_est_min)
+    print("E_in = %f, s = %d, t = %f, i = %d" % (err / len(d_train), s, t, i))
+
+    d_test = read_data("data/hw2_test.dat")
+    err = 0
+    for d in d_test:
+        if s * sgn(d[i] - t) != d[dim]: err = err + 1
+    print("est. E_out = %f" % (err / len(d_test)))
+
+
 def main():
     fmap = {
         '17': prob1718,
         '18': prob1718,
+        '19': prob1920,
+        '20': prob1920,
     }
     try:
         fmap[sys.argv[1]]()
